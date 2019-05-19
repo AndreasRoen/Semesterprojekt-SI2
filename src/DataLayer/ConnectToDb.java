38fb30191caca2ba01c5fdd6d6e4419e2b114337
Maltebuk <33434289+Maltebuk@users.noreply.github.com>
2019-05-19 19:43:39 +0200

    DB Changes

--- src/DataLayer/ConnectToDb.java
+++ src/DataLayer/ConnectToDb.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package DataLayer;
-import java.sql.*; 
+import java.sql.*;
 
 /**
  *
@@ -13,18 +13,37 @@ import java.sql.*;
 public class ConnectToDb
 {
     
+    // ALL PARAMTERES HAS TO BE SET IN THE INTERFACE FOR THE USE OF ADMINS ONLY 
+    public static final String DB_NAME = "postgres";
+    public static final String CONNECTION_STRING = "jdbc:postgresql://localhost:5432/" + DB_NAME; 
+    public static final String TABLE_NAME = "contacts"; 
+    public static final String COLUMN_NAME = "Blabla"; 
+    public static final int COLUMN_PERSON_ID = 0; 
+    public static final String COLUMN_LASTNAME = "Blabla"; 
+    public static final String COLUMN_ROLEID = "Blabla"; 
+    public static final String COLUMN_KOMMUNE = "Blabla"; 
+    public static final String COLUMN_TS = "Blabla"; 
+    public static final String COLUMN_BOSTED = "Blabla"; 
+    public static final String COLUMN_PASSWORD = "Blabla"; 
+ 
+    private Connection connection; 
+    
+    
     public void connection(){
-//    try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","ASDqwe123"); // Username and password for your Database :) 
-//        Statement statement = conn.createStatement();){        
+//    try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","ASDqwe123"); // Username and password for your Database :) 
+//        Statement statement = connection.createStatement();){        
 //        statement.execute("CREATE DATABASE SOCIALPORTALEN"); 
     try{
-        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","ASDqwe123"); // Username and password for your Database :) 
-        conn.setAutoCommit(true); // Set false if Auto commit to DB is not wanted (It is guys ;P ) 
-        Statement statement = conn.createStatement();
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","ASDqwe123"); // Username and password for your Database :) 
+        connection.setAutoCommit(true); // Set false if Auto commit to DB is not wanted (It is guys ;P ) 
+        Statement statement = connection.createStatement();
         statement.execute("CREATE DATABASE IF NOT EXISTS SOCIALPORTALEN"); 
         
-        statement.execute("SELECT * FROM DB");
-        ResultSet result = statement.getResultSet();
+//        statement.execute("SELECT * FROM DB");
+//        ResultSet result = statement.getResultSet();
+        ResultSet result = statement.executeQuery("SELECT * from "+ TABLE_NAME); 
+        
+        
         while (result.next())
         {
             System.out.println(result.getString("name"));  // A way to print out a statement            
@@ -32,11 +51,74 @@ public class ConnectToDb
         
         
         statement.close();
-        conn.close();
+        connection.close();
     }catch(SQLException e) {
         System.out.println("Somehing went wrong "+ e.getMessage());
     }
    
     
 }
+    
+//    private void executeSQL(){
+//        if(connection == null){
+//            System.out.println("Please connect to a database first"); // Should be printed to the GUI
+//            return;
+//        }else{
+//            
+//        }
+//    }
+//    
+    private static void addUser(Statement statement,int Person_ID, String name, String lastName, int role_ID, String SocialPortalenTs) throws SQLException{
+        statement.execute("INSERT INTO " + TABLE_NAME +
+                " ("+COLUMN_PERSON_ID +", "
+                 + "" + COLUMN_NAME + ", "
+                 + "" + COLUMN_LASTNAME + ", "
+                 + "" + COLUMN_ROLEID + " , "
+                 + "" + COLUMN_TS +
+                 ") " +
+                "VALUES(" + Person_ID + ", '" + name + "', '" + lastName + "'," + role_ID + ",'" + SocialPortalenTs + "')");
+        
+    }
+    
+    private void addUserInformation(Statement statement,int Person_ID, String password, String Kommune1,String bosted1, int role_ID, String SocialPortalenTs) throws SQLException{
+            statement.execute("INSERT INTO " + TABLE_NAME +
+                " ("+COLUMN_PERSON_ID +", "
+                 + "" + COLUMN_PASSWORD + ", "
+                 + "" + COLUMN_KOMMUNE + ", "
+                 + "" + COLUMN_BOSTED + " , "
+                 + "" + COLUMN_ROLEID + " , "        
+                 + "" + COLUMN_TS +
+                 ") " +
+                "VALUES(" + Person_ID + ", '" + password + "', '" + Kommune1 + "',, '" + bosted1 + "'" + role_ID + ",'" + SocialPortalenTs + "')");        
+        
+    }
+    
+    private void addStaff(Statement statement,int Person_ID, String password, String Kommune1,String bosted1, int role_ID, String SocialPortalenTs)throws SQLException{
+            statement.execute("INSERT INTO " + TABLE_NAME +
+                " ("+COLUMN_PERSON_ID +", "
+                 + "" + COLUMN_PASSWORD + ", "
+                 + "" + COLUMN_KOMMUNE + ", "
+                 + "" + COLUMN_BOSTED + " , "
+                 + "" + COLUMN_ROLEID + " , "        
+                 + "" + COLUMN_TS +
+                 ") " +
+                "VALUES(" + Person_ID + ", '" + name + "', '" + lastName + "'," + role_ID + ",'" + SocialPortalenTs + "')");    //TODO set in the right parameters for staff     
+        
+    }
+    private void addResident(Statement statement,int Person_ID, String password, String Kommune1,String bosted1, int role_ID, String SocialPortalenTs)throws SQLException{
+            statement.execute("INSERT INTO " + TABLE_NAME +
+                " ("+COLUMN_PERSON_ID +", "
+                 + "" + COLUMN_PASSWORD + ", "
+                 + "" + COLUMN_KOMMUNE + ", "
+                 + "" + COLUMN_BOSTED + " , "
+                 + "" + COLUMN_ROLEID + " , "        
+                 + "" + COLUMN_TS +
+                 ") " +
+                "VALUES(" + Person_ID + ", '" + name + "', '" + lastName + "'," + role_ID + ",'" + SocialPortalenTs + "')");        //TODO set in the right parameters for residents      
+        
+    }
+    
+    
+    
+    
 }
