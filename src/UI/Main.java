93839410bddd4d1e7caa68efbf2384b5cffe1052
dafar18 <43138769+dafar18@users.noreply.github.com>
2019-05-20 03:14:37 +0200

    EventHandler placeholder

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -428,6 +428,11 @@ public class Main extends Application {
                 listView.getItems().remove(listView.getSelectionModel().getSelectedItem());
             }
         });
-        
+        autoGen.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
     }
 }
