92a5d28198131feebfd8f44cf2a3096cb7a1f7c3
Maltebuk <33434289+Maltebuk@users.noreply.github.com>
2019-05-25 19:57:36 +0200

    Added Create database script, all we need now is premade users

--- src/DataLayer/DatabaseManager.java
+++ src/DataLayer/DatabaseManager.java
@@ -181,7 +181,41 @@ public class DatabaseManager{
             System.out.println(ex.getMessage());
         }
     }
-        public void createDatabase()
+      public void CreateUser(String username, String password){
+          String SQL = "Insert into Login_info(username, pass) "
+                + "FROM Bruger "
+                + "WHERE kommune = ?"
+                ;
+
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(SQL))
+        {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            displayUser(rs);
+        } catch (SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+          
+      }
+      
+      public boolean validatePassword(String Username, String password){
+          
+          
+          return true; 
+      }
+        
+      
+      
+      
+      
+      
+      
+      
+      public void createDatabase()
     {
         String SQL = "CREATE DATABASE \"Socialportalen\"\n" +
         "    WITH \n" +
@@ -195,48 +229,66 @@ public class DatabaseManager{
         "GRANT ALL ON DATABASE \"Socialportalen\" TO postgres;\n" +
         "\n" +
         "GRANT TEMPORARY, CONNECT ON DATABASE \"Socialportalen\" TO PUBLIC;"
-                + "Create Table if not exists User(\n" +
+                
+      + "Create Table if not exists Users(\n" +
         "    Person_id int not null PRIMARY KEY,\n" +
         "    Name varchar(130) not null ,\n" +
         "    lastName varchar(200) not null,\n" +
         "    Role_id int not null,\n" +
         "    Socialportalen_Ts char(26)	\n" +
         "); \n" +
+        "INSERT INTO Users(\n" +
+"	person_id, name, lastname, role_id, socialportalen_ts)\n" +
+"	VALUES \n" +
+"	(1, 'Admin','', 3, '01-01-2000'),\n" +
+"	(2, 'Malte', 'Bukrinski', 3, '06-01-1995'),\n" +
+"	(3, 'Per', 'Frederiksen', 1, '13-03-1993'),\n" +
+"	(4, 'Peter', 'Andersen', 2, '25-06-1992'),\n" +
+"	(5, 'Anders', 'Jordan', 2, '13-05-1992'),\n" +
+"	(6, 'Preben', 'Ravi', 3, '24-04-1992'),\n" +
+"	(7, 'Mads', 'Christensen', 1, '06-03-1992'),\n" +
+"	(8, 'Mikkel', 'Petersen', 2, '12-12-1992'),\n" +
+"	(9, 'Admir', 'Pedersen', 1, '11-11-1992');\n" +  
+                
+                
         "	\n" +
-        "Create Table if not exists User_information(\n" +
-        "	Person_id int not null PRIMARY KEY,\n" +
-        "	Password varchar(130) not null ,\n" +
-        "	Kommune1 varchar(200) not null,\n" +
-        "	Kommune2 varchar(200),\n" +
-        "	Kommune3 varchar(200),\n" +
-        "	Bosted1 Varchar(200) not null,\n" +
-        "	Bosted2 Varchar(200),\n" +
-        "	Bosted3 Varchar(200),\n" +
-        "	Socialportalen_Ts char(26)\n" +
-        "); \n" +
-        "\n" +
+                
+                
         "\n" +
         "CREATE TABLE if not exists staff(\n" +
         "    Person_id int not null PRIMARY KEY,\n" +
         "    Kommune1 varchar(200) not null,\n" +
-        "	Kommune2 varchar(200),\n" +
-        "	Kommune3 varchar(200),\n" +
-        "	Bosted1 Varchar(200) not null,\n" +
-        "	Bosted2 Varchar(200),\n" +
-        "	Bosted3 Varchar(200),\n" +
+        "    Kommune2 varchar(200),\n" +
+        "    Kommune3 varchar(200),\n" +
+        "    Bosted1 Varchar(200) not null,\n" +
+        "    Bosted2 Varchar(200),\n" +
+        "    Bosted3 Varchar(200),\n" +
         "    Role_id int not null,\n" +
         "    Socialportalen_Ts char(26)\n" +
         "\n" +
         ");\n" +
+                
+                
+                
         "\n" +
         "CREATE TABLE if not exists residents(\n" +
         "    Person_id int not null PRIMARY KEY,\n" +
         "    Kommune1 varchar(200) not null,\n" +
-        "	Bosted1 Varchar(200) not null,\n" +
-        "    PermissionLevel int,\n" +
+        "    Bosted1 Varchar(200) not null,\n" +
+        "    Role_id  int not null,\n" +
         "    Socialportalen_Ts char(26)\n" +
         "\n" +
-        ");"
+        ");" +
+                
+                
+        "CREATE TABLE if not exists Login_info \n " +
+        "    Person_id int not null PRIMARY KEY , \n" +
+        "    Username varchar(200) not null,\\n\" +\n" +
+        "    Pass Varchar(200) not null,\\n\" +\n" +
+        " +\n" +
+        ");" 
+                
+                
                 
         ;
 
@@ -251,33 +303,7 @@ public class DatabaseManager{
         }
         
     }
-        public void createTableUsers()
-    {
-        String SQL = "CREATE Table \"\"\n" +
-        "    WITH \n" +
-        "    OWNER = postgres\n" +
-        "    ENCODING = 'UTF8'\n" +
-        "    LC_COLLATE = 'Danish_Denmark.1252'\n" +
-        "    LC_CTYPE = 'Danish_Denmark.1252'\n" +
-        "    TABLESPACE = pg_default\n" +
-        "    CONNECTION LIMIT = -1;\n" +
-        "\n" +
-        "GRANT ALL ON DATABASE \"Socialportalen\" TO postgres;\n" +
-        "\n" +
-        "GRANT TEMPORARY, CONNECT ON DATABASE \"Socialportalen\" TO PUBLIC;"
-        ;
-
-        try (Connection conn = connect();
-                PreparedStatement pstmt = conn.prepareStatement(SQL))
-        {
-            ResultSet rs = pstmt.executeQuery();
-           
-        } catch (SQLException ex)
-        {
-            System.out.println(ex.getMessage());
-        }
-        
-    }
+ 
         
 
 
